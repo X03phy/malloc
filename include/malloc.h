165f7ab1442834ac0007a30d41e33f4bf1be5a29
Elio Bonutto <ebonutto@xmco-ebon.office.xmco.fr>
2026-05-07 17:44:02 +0200

    feat: add structure

--- include/malloc.h
+++ include/malloc.h
@@ -1,6 +1,20 @@
 #ifndef MALLOC_H
 #define MALLOC_H
 
+/* Includes */
 
+#include <stddef.h> // size_t
+
+/* Structures */
+
+struct s_chunk {
+	int free;
+	size_t size;
+	struct s_chunk *next, *prev;
+} s_chunk;
+
+/* Prototypes */
+
+void *malloc(size_t size);
 
 #endif
